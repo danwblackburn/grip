6c73f908bfa7e024ac9be87935b425fa4ae31773
Danny B <danwb@stanford.edu>
2023-05-25 18:01:44 -0700

    Add Gripper State to leader

--- leader/leader.ino
+++ leader/leader.ino
@@ -1,6 +1,9 @@
 ///--------------------------------------------------------------------------
-// Code to test basic Hapkit functionality (sensing and force output)
-// Updated by Allison Okamura 1.17.2018, Nathan Kau 9.9.2022
+// 2023 Spring ME 327 Project
+// Blake Jones, Danny Blackburn, Ashwin Vangipuram, Ankitha Durvasula
+//
+// Forked by Danny Blackburn 5.24.2023 
+// From code by Allison Okamura 1.17.2018, Nathan Kau 9.9.2022
 //--------------------------------------------------------------------------
 
 //#define HALL_EFFECT_SENSOR
@@ -8,6 +11,7 @@
 
 #include <math.h>
 #include "helpers.h"
+#include "GripperState.h"
 
 #define BAUDRATE 115200
 
@@ -16,50 +20,30 @@ enum class Mode {
     scaled_bilateral
 };
 
+// Set current mode
 Mode current_mode = Mode::bilateral;
 
+// Create new state
+GripperState current_state;
 
 /*
-* Map from position reading to angle in radians.
+* Map from mr counts reading to sector angle in radians.
 */
-double pos_to_angle (double pos)
+double MRToSectorAngle (double mr_counts)
 {
-  
   // Linear fit in degrees
  double m = -0.011283;
  double b = 7.749591;
- double deg = m * pos + b;
+ double deg = m * mr_counts + b;
+
  return deg * M_PI / 180.0; //deg to rad
-}
-
-/*
- * Calculates the position of the hapkit handle given the sensor reading
- */
-float calculate_handle_position(float updated_position) {
-  double rh = .075;                       // m 
-  double angle = pos_to_angle(updated_position);
-  float xh = rh * angle;                        // x = r * theta
-
-  return xh;
-}
-
-/*
- * Calculates the pulley torque necessary to achieve the desired force
- */
-float calculate_pulley_torque(float force) {
-  double rh = 0.075;   //[m]
-  double rs = 0.075;   //[m]
-  double rp = 0.0095; //[m]
-  float Tp = rh * rp / rs * force;
-  return Tp;
 }
 
 /*
  * Specifies a desired force to render on the hapkit
  */
-float calculate_force(float handle_position, float handle_velocity, float handle_position_remote) {
-  // STUDENT CODE HERE
-
+float calculate_force(float handle_position, float handle_velocity, float handle_position_remote) 
+{
   float handle_velocity_remote = 0;
   float kp = 100;
   float kd = 1;
@@ -91,20 +75,21 @@ int count = 0;
 void loop()
 {
   // Compute position in counts
-  float updated_position = read_sensor();
+  float mr_counts = read_sensor();
 
-  // Compute position [m] and velocity [m/s]
-  float handle_position = calculate_handle_position(updated_position);
-  float smoothed_velocity = calculate_smoothed_velocity(handle_position, /*DT=*/0.001);
+  // Update state with newest reading
+  current_state.UpdateState(MRToSectorAngle(mr_counts), /*DT=*/0.001);
   
-  float remote_position = send_receive_remote_arduino(handle_position);
-  
-  float force = calculate_force(handle_position, smoothed_velocity, remote_position);
-  float pulley_torque = calculate_pulley_torque(force);
-  
-  // Command the motor
-  command_motor(pulley_torque);
+  // Receive leader position
+  float remote_position = send_receive_remote_arduino(current_state.GetFingerPos());
 
-  // Check if your loop speed is too slow, and if so, print an error message.
+  // Calculate and set force
+  float force = calculate_force(current_state.GetFingerPos(), smoothed_velocity, remote_position);
+  current_state.SetDesiredForce(force);
+
+  // Command motor torque
+  command_motor(current_state.GetTorqueCmd());
+
+  // Check if loop speed is too slow, and if so, print an error message.
   check_loop_speed();
 }
